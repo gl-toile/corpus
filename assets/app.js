0687ed8cc9d4c98d3b432716b5473909a68cb634
Guillaume Larroque <larroque.guillaume@gmail.com>
2022-04-05 20:31:21 +0200

    Ajout de l'affichage en mode tableau

--- assets/app.js
+++ assets/app.js
@@ -5,11 +5,21 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import './bootstrap';
+
 // any CSS you import will output into a single css file (app.css in this case)
+//import './vendor/fonts/boxicons.css';
+import './vendor/fonts/boxicons.css'
+import './vendor/css/core.css';
+import './vendor/css/theme-default.css';
+import './css/demo.css';
+import './vendor/libs/perfect-scrollbar/perfect-scrollbar.css';
+import './vendor/libs/apex-charts/apex-charts.css';
+
 import './styles/app.scss';
 
 // You can specify which plugins you need
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
-import './bootstrap';
+import './vendor/js/helpers.js';
